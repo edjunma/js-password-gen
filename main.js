246f97f5a79f28d845c9d0ee8476c1e003ea6267
edjunma <edjunma@gmail.com>
2019-10-23 03:39:18 -0400

    first commit, setting up files

---
+++ main.js
@@ -0,0 +1,9 @@
+// Generator Functions - http://www.net-comber.com/charset.html
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
