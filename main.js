b0cf3a3fa4276f141d3d56de9f275b56747f5d90
edjunma <edjunma@gmail.com>
2019-10-23 03:41:19 -0400

    added random number and symbol functions

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ function getRandomLower() {
 function getRandomUpper() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
+function getRandomNumber() {
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.';
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
